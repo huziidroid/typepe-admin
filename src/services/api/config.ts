4c4e1075ce48d35a33fbd42668bdb0150937dc02
Huzaifa-Personal <huzaifa.arshad1911@gmail.com>
2023-10-14 23:01:22 +0500

    Updated Server baseurl

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -4,10 +4,10 @@ import {StorageService} from '..';
 import {AUTH_TOKEN_KEY, isEmptyOrNil} from '@/utils';
 
 // staging
-// const BASE_URL = 'http://13.233.201.212:1108/';
+const BASE_URL = 'http://13.233.201.212:1108/';
 
 // local
-const BASE_URL = 'http://localhost:1108/';
+// const BASE_URL = 'http://localhost:1108/';
 
 export const queryClient = new QueryClient({
   defaultOptions: {
